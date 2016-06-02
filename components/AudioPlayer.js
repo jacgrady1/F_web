9ca8fb44eb35dcc50ce7db02e8c5eb7ef8914299
Jack Liu <gongxun@yahoo-inc.com>
2016-06-01 18:15:29 -0700

    add timeline

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -1,18 +1,42 @@
 import React from 'react';
+import {MdPlayArrow, MdPause} from 'react-icons/lib/md';
 
 class Audioplayer extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            playing: false
+        };
         this.handlePlayClicked = this.handlePlayClicked.bind(this);
     }
     handlePlayClicked () {
         var music = this.refs.music;
         if (music.paused) {
             music.play();
+            this.setState({playing: true});
         } else {
             music.pause();
+            this.setState({playing: false});
         }
-
+    }
+    renderButton() {
+        if (this.state.playing === true) {
+            return (<div>
+                        <MdPause className="pause Fz(200%) Scale(2.0, 2.0)" onClick={this.handlePlayClicked} />
+                    </div>);
+        } else {
+            return (<div>
+                        <MdPlayArrow className="play Fz(200%) Scale(2.0, 2.0)" onClick={this.handlePlayClicked} />
+                    </div>);
+        }
+    }
+    renderTimeLine() {
+    //     border-radius: 15px;
+	// background: rgba(0,0,0,.3);
+    // dark color #26a69a
+        return (<div id="timeline" className = "W(400px) H(20px) Mt(20px) Fl(start) Bdrs(15px) Bgc(#acece6)" onClick={this.onTimelineClick}>
+                    <div ref="playhead" className="playhead"></div>
+                </div>);
     }
     render() {
         return (
@@ -25,12 +49,9 @@ class Audioplayer extends React.Component {
                             <p>Your browser does not support the <code>audio</code> element </p>
                             </audio>
                             <div id="audioplayer">
-                                    <button type="button" className="btn btn-default" aria-label="Left Align" onClick={this.handlePlayClicked}>
-                                        <span className="glyphicon glyphicon-play" aria-hidden="true"></span>
-                                    </button>
-                                <div ref="timeline" id="timeline" onClick={this.onTimelineClick}>
-                                            <div ref="playhead" className="playhead"></div>
-                                    </div>
+                                {this.renderButton()}
+                                {this.renderTimeLine()}
+
                             </div>
                         </div>
                     </div>
