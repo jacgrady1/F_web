8695c4ccf6cc7e55fb5c2c467c3c1424316f1312
Jack Liu <gongxun@yahoo-inc.com>
2016-09-29 21:55:44 -0700

    remove unused tabs

--- configs/routes.js
+++ configs/routes.js
@@ -1,23 +1,23 @@
 export default {
-    home: {
-        path: '/',
-        method: 'get',
-        page: 'home',
-        title: 'Home',
-        handler: require('../components/Home')
-    },
+    // home: {
+    //     path: '/',
+    //     method: 'get',
+    //     page: 'home',
+    //     title: 'Home',
+    //     handler: require('../components/Home')
+    // },
     video: {
         path: '/video',
         method: 'get',
         page: 'video',
         title: 'Video',
         handler: require('../components/Video')
-    },
-    audio: {
-        path: '/audio',
-        method: 'get',
-        page: 'audio',
-        title: 'Audio',
-        handler: require('../components/Audio')
     }
+    // audio: {
+    //     path: '/audio',
+    //     method: 'get',
+    //     page: 'audio',
+    //     title: 'Audio',
+    //     handler: require('../components/Audio')
+    // }
 };
