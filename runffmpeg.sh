cd43dee28c7e43f5846ed86fb17c07551cbc41c0
Jack Liu <gongxun@yahoo-inc.com>
2016-10-14 21:00:41 -0700

    font change

--- runffmpeg.sh
+++ runffmpeg.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -i $2 -ss $4 -t $5 -vf drawtext="fontfile=Xpressive.ttf: text='$1': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5: boxborderw=5: x=(w-text_w)/10: y=(h-text_h)*4/5" -codec:a copy -async 1 -strict -2 $3 -y
+ffmpeg -i $2 -ss $4 -t $5 -vf drawtext="fontfile=NotoSansCJKtc-Regular.otf: text='$1': fontcolor=white: fontsize=24: box=1: boxcolor=black@0.5: boxborderw=5: x=(w-text_w)/10: y=(h-text_h)*4/5" -codec:a copy -async 1 -strict -2 $3 -y
